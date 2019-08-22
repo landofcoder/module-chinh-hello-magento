3160083a6d87f206aa4b8398a7a1667e0b02421a
HaNguyenQuang <nguyenquangha0209@gmail.com>
2019-08-22 10:07:28 +0700

    add system setting for page display: title and display_text

--- Block/Display.php
+++ Block/Display.php
@@ -1,18 +1,47 @@
 <?php
 namespace Lof\ChinhHelloMagento\Block;
+
+use Lof\ChinhHelloMagento\Helper\Data;
+use Magento\Framework\View\Element\Template;
+
 class Display extends \Magento\Framework\View\Element\Template
 {
-	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	protected $helperData;
+	public function __construct(\Magento\Framework\View\Element\Template\Context $context,
+	\Lof\ChinhHelloMagento\Helper\Data $helperData,
+	\Magento\Framework\View\Result\PageFactory $pageFactory)
 	{
 		parent::__construct($context);
+		$this->helperData = $helperData;
+		$this->_pageFactory = $pageFactory;
 	}
 	public function sayHello()
-	{
-        return __('<H1>Chinh Hello Magento</H1>
+	{	
+		return __('<H1>Chinh Hello Magento</H1>
+		<?php
+		echo "";
+		?>
         <style>
         H1{
-            color: red;
+            color: blue;
         }
-        </style>');
+		</style>');
+		return $this->_pageFactory->create();
 	}
+	public function GetDisplayText()
+    {
+		$textdisplay = $this->helperData->getConfigDisplayText();
+		$enable = $this->helperData->getConfigEnable();
+		if($enable == 1){
+			return $textdisplay;
+		}
+	}
+	public function GetTitle()
+    {
+		$title = $this->helperData->getConfigTitle();
+		$enable = $this->helperData->getConfigEnable();
+		if($enable == 1){
+			return $title;
+		}
+    }
 }
--- Controller/Index/Display.php
+++ Controller/Index/Display.php
@@ -1,20 +1,35 @@
 <?php
 namespace Lof\ChinhHelloMagento\Controller\Index;
+use Lof\ChinhHelloMagento\Helper\Data;
+
 class Display extends \Magento\Framework\App\Action\Action
 {
 	protected $_pageFactory;
+
+	protected $helperData;
+
 	public function __construct(
 		\Magento\Framework\App\Action\Context $context,
-		\Magento\Framework\View\Result\PageFactory $pageFactory)
+		\Magento\Framework\View\Result\PageFactory $pageFactory,
+		\Lof\ChinhHelloMagento\Helper\Data $helperData
+		)
 	{
+		
+		parent::__construct($context);
 		$this->_pageFactory = $pageFactory;
-		return parent::__construct($context);
+		$this->helperData = $helperData;
 	}
 	public function execute()
 	{
+		if($this->helperData->getGeneralConfig('enable') == 1){
+			?>
+			<head>
+				<title>
+					<?php echo $this->helperData->getGeneralConfig('title_text');?>
+				</title>
+			</head>
+			<?php
+		}
 		return $this->_pageFactory->create();
-		$this->resultPage = $this->resultPageFactory->create();  
-  		$this->resultPage->getConfig()->getTitle()->set((__('Welcome to Codesspires')));
-  		return $this->resultPage;
 	}
 }
